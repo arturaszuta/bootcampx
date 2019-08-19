ae23e4d68bb5049ef5f895cfd67281627a9c953f
Arturas Zuta <arturaszuta@gmail.com>
2019-08-19 17:46:31 +0000

    Add query for rollover students

---
+++ 1_queries/7_rollover_students.sql
@@ -0,0 +1,5 @@
+SELECT students.name AS students_name, students.start_date AS students_start_date, cohorts.name AS cohorts_name, cohorts.start_date AS cohorts_start_date
+FROM students
+LEFT JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE students.start_date <> cohorts.start_date
+ORDER BY cohorts_name;
